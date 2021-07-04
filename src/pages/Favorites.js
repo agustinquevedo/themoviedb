e43ebe0b53ce8dd18d8a0dafae4f64645b73158f
Agustin Quevedo <you@example.com>
2021-07-04 16:16:10 +0200

    Rendering favoritesList in FavoritesPage

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,41 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
 const Favorites = () => {
+    const { favoritesList } = useContext(GlobalContext)
+
     return (
-    <div className="container">
-        <h2 className="title is-2 is-size-4-mobile">Favorites</h2>
+    <div className="container pt-2 pb-2">
+        <h2 className="title is-3 is-size-5-mobile">My favorites movies</h2>
+        <div className="columns is-multiline">
+            {
+                favoritesList.map(movie => (
+                    <div className="column is-one-quarter is-full-mobile">
+                        <div className="card" key={movie.id}>
+                            <div className="card-image">
+                                <figure className="image is-2by3">
+                                    {
+                                        movie.poster_path ? (
+                                            <img 
+                                                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} 
+                                                alt={`${movie.title} Poster`}
+                                                
+                                            />
+                                        ) : (
+                                            <img 
+                                                src={"https://via.placeholder.com/200x300/0D253F/FFFFFF?text=Poster+not+found"}
+                                                alt={"Poster not found"}
+                                            />
+                                        )
+                                    }
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+            
+        </div>
     </div>
     );
 }
